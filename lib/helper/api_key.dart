d4c69b56da26905cbb54703c5e4981b51bd2707e
ro77en <lucas.mdantas@icloud.com>
2024-06-07 19:46:33 -0300

    free gpt apikey

---
+++ lib/helper/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = 'sk-proj-OS86waHFc9Ca3My2vrXUT3BlbkFJkKi7bLn2gTzY1pimNey0';
--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -3,5 +3,3 @@ import 'package:flutter/material.dart';
 const appName = 'AI Assistant';
 
 late Size mq;
-
-const apiKey = 'sk-proj-OS86waHFc9Ca3My2vrXUT3BlbkFJkKi7bLn2gTzY1pimNey0';
